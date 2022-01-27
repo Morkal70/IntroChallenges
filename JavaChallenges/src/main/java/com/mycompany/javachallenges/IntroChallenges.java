340a03f1b2930225b2415a57b5713c17b5d6d41f
Toon <M1001886@middlesbro.ac.uk>
2022-01-27 12:10:04 +0000

    initial commit

---
+++ JavaChallenges/src/main/java/com/mycompany/javachallenges/IntroChallenges.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.javachallenges;
+
+/**
+ *
+ * @author M1001886
+ */
+public class IntroChallenges {
+    public static void helloWorld(){
+        System.out.println("Hello World");
+    }
+    
+    
+
+
+public static void outputNumber(){
+    int num1;
+    num1 = 7;
+    System.out.println(num1);
+    
+   
+       
+   
+}
+
+}
---
+++ JavaChallenges/src/main/java/com/mycompany/javachallenges/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.javachallenges;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author M1001886
+ */
+public class Main {
+    static Scanner SC = new Scanner(System.in);
+    //
+    
+   public static void main (String[] args) {
+       //IntroChallenges.helloWorld();
+       IntroChallenges.outputNumber();
+   }
+}
